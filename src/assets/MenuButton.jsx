bf6333161179f22b374395e81916995775139326
SilverCloud <obj268version4@gmail.com>
2025-12-19 11:19:45 +0530

    Open Close Animation Fix

--- src/assets/MenuButton.jsx
+++ src/assets/MenuButton.jsx
@@ -6,7 +6,7 @@ const MenuButton = ({ isOpen, toggleMenu }) => {
     >
       <div className="relative h-6 overflow-hidden text-right">
         <div
-          className={`flex flex-col transition-transform duration-500 ease-[cubic-bezier(0.76,0,0.24,1)] ${
+          className={`flex flex-col transition-transform duration-1250 ease-[cubic-bezier(0.25,1,0.5,1)] ${
             isOpen ? "-translate-y-1/2" : "translate-y-0"
           }`}
         >
@@ -14,20 +14,23 @@ const MenuButton = ({ isOpen, toggleMenu }) => {
           <span className="h-6 flex items-center">Close</span>
         </div>
       </div>
+
       <div className="flex flex-col justify-between items-center w-6 h-3.5">
         <span
-          className={`w-6 h-0.5 bg-white transition-all duration-300 ease-out origin-center ${
+          className={`w-6 h-0.5 bg-white transition-all duration-1000 ease-[cubic-bezier(0.25,1,0.5,1)] origin-center ${
             isOpen ? "rotate-45 translate-y-1.5" : ""
           }`}
         />
+        
         <span
-          className={`w-6 h-0.5 bg-white transition-all duration-300 ease-out ${
+          className={`w-6 h-0.5 bg-white transition-all duration-1000 ease-[cubic-bezier(0.25,1,0.5,1)] ${
             isOpen ? "opacity-0" : "opacity-100"
           }`}
         />
+
         <span
-          className={`w-6 h-0.5 bg-white transition-all duration-300 ease-out origin-center ${
-            isOpen ? "-rotate-45 -translate-y-1.5" : ""
+          className={`w-6 h-0.5 bg-white transition-all duration-1000 ease-[cubic-bezier(0.25,1,0.5,1)] origin-center ${
+            isOpen ? "-rotate-45 -translate-y-1.5]" : ""
           }`}
         />
       </div>
